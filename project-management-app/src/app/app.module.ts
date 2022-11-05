3547a5fd9db0d1e5de2f7e214927cd0760504a05
vzakharenkova <v.s.zakharenkova@gmail.com>
2022-11-05 11:06:09 +0300

    feat: create shared module

--- project-management-app/src/app/app.module.ts
+++ project-management-app/src/app/app.module.ts
@@ -1,17 +1,16 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
+import { SharedModule } from './shared/shared.module';
+
 import { AppComponent } from './app.component';
 import { WelcomePageComponent } from './welcome-page/welcome-page.component';
 import { TitleComponent } from './welcome-page/components/title/title.component';
 import { TeamComponent } from './welcome-page/components/team/team.component';
 import { TeamCardComponent } from './welcome-page/components/team-card/team-card.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { DescriptionComponent } from './welcome-page/components/description/description.component';
-
-import { MatButtonModule } from '@angular/material/button';
-import { MatIconModule } from '@angular/material/icon';
 
 @NgModule({
   declarations: [
@@ -22,13 +21,7 @@ import { MatIconModule } from '@angular/material/icon';
     TeamCardComponent,
     DescriptionComponent,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    BrowserAnimationsModule,
-    MatButtonModule,
-    MatIconModule,
-  ],
+  imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule, SharedModule],
   providers: [],
   bootstrap: [AppComponent],
 })
