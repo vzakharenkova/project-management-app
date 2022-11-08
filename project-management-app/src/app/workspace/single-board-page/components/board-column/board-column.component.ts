51cc2f9072e43ca6279a88af01757f56f629d172
rybakN <dnb4949@gmail.com>
2022-11-08 19:49:46 +0300

    refactor: task-form.models.ts, method openTaskForm

--- project-management-app/src/app/workspace/single-board-page/components/board-column/board-column.component.ts
+++ project-management-app/src/app/workspace/single-board-page/components/board-column/board-column.component.ts
@@ -1,7 +1,8 @@
 import { Component, Input } from '@angular/core';
-import { ColumnModel } from 'src/app/workspace/board-list-page/models/board.model';
+import { ColumnModel, TaskPriority, TaskSize } from 'src/app/workspace/board-list-page/models/board.model';
 import { MatDialog } from '@angular/material/dialog';
 import { TaskFormComponent } from '../../../task-form/task-form.component';
+import { TaskForm } from '../../../task-form/models/task-form.models';
 
 @Component({
   selector: 'app-board-column',
@@ -11,10 +12,23 @@ import { TaskFormComponent } from '../../../task-form/task-form.component';
 export class BoardColumnComponent {
   @Input() column: ColumnModel;
 
+  private taskFormConfig: TaskForm;
+
   constructor(private dialog: MatDialog) {
   }
 
   openTaskForm() {
-    this.dialog.open(TaskFormComponent);
+    this.taskFormConfig = {
+      title: 'Create Task',
+      btnName: 'Create Task',
+      submitBtn: () => console.log('Создано!'),
+      formFields: {
+        taskSize: TaskSize.TINY,
+        taskPriority: TaskPriority.HIGH,
+      }
+    };
+    this.dialog.open(TaskFormComponent, {
+      data: this.taskFormConfig,
+    });
   }
 }
--- project-management-app/src/app/workspace/single-board-page/components/task-card/task-card.component.ts
+++ project-management-app/src/app/workspace/single-board-page/components/task-card/task-card.component.ts
@@ -2,6 +2,7 @@ import { Component, Input } from '@angular/core';
 import { TaskModel } from 'src/app/workspace/board-list-page/models/board.model';
 import { MatDialog } from '@angular/material/dialog';
 import { TaskFormComponent } from '../../../task-form/task-form.component';
+import { TaskForm } from '../../../task-form/models/task-form.models';
 
 @Component({
   selector: 'app-task-card',
@@ -11,10 +12,25 @@ import { TaskFormComponent } from '../../../task-form/task-form.component';
 export class TaskCardComponent {
   @Input() task: TaskModel;
 
+  private taskFormConfig: TaskForm;
+
   constructor(private dialog: MatDialog) {
   }
 
   openTaskForm() {
-    this.dialog.open(TaskFormComponent)
+    this.taskFormConfig = {
+      title: 'Edite Task',
+      btnName: 'Edit Task',
+      submitBtn: () => console.log('Отредактировано!'),
+      formFields: {
+        taskName: this.task.title,
+        taskSize: this.task.size,
+        taskPriority: this.task.priority,
+        taskDescription: this.task.description
+      }
+    };
+    this.dialog.open(TaskFormComponent, {
+      data: this.taskFormConfig,
+    })
   }
 }
