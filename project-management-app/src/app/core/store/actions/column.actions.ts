77cf64cd7a3cac9869380866c399ea891e0e120b
Pavel Kizhlo <pavelkizh@gmail.com>
2022-11-08 12:32:43 +0300

    feat: create actions for effects

---
+++ project-management-app/src/app/core/store/actions/column.actions.ts
@@ -0,0 +1,31 @@
+import { createAction, props } from '@ngrx/store';
+
+export enum ColumnActionsList {
+  getAll = '[COLUMN] Get all columns',
+  create = '[COLUMN] Create new column',
+  getById = '[COLUMN] Get one column',
+  delete = '[COLUMN] Delete column',
+  update = '[COLUMN] Update column',
+}
+
+export const getAllColumns = createAction(ColumnActionsList.getAll, props<{ boardId: string }>());
+
+export const createColumn = createAction(
+  ColumnActionsList.create,
+  props<{ boardId: string; title: string }>(),
+);
+
+export const getColumnById = createAction(
+  ColumnActionsList.getById,
+  props<{ boardId: string; columnId: string }>(),
+);
+
+export const deleteColumn = createAction(
+  ColumnActionsList.delete,
+  props<{ boardId: string; columnId: string }>(),
+);
+
+export const updateColumn = createAction(
+  ColumnActionsList.update,
+  props<{ boardId: string; columnId: string; data: { title: string; order: number } }>(),
+);
---
+++ project-management-app/src/app/core/store/actions/user.actions.ts
@@ -0,0 +1,16 @@
+import { createAction, props } from '@ngrx/store';
+
+export enum UserActionsList {
+  getAll = '[USER] Get all users',
+  getById = '[USER] Get one user',
+  delete = '[USER] Delete user',
+  update = '[USER] Update user',
+}
+
+export const getAllUsers = createAction(UserActionsList.getAll);
+
+export const getUserById = createAction(UserActionsList.getById, props<{ userId: string }>());
+
+export const deleteUser = createAction(UserActionsList.delete, props<{ userId: string }>());
+
+export const updateUser = createAction(UserActionsList.update, props<{ userId: string }>());
