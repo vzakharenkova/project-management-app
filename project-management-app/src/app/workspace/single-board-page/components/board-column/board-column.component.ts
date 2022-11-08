bac7d9764a9fd4385b838710c1ca51840de04bb9
vzakharenkova <v.s.zakharenkova@gmail.com>
2022-11-08 16:01:11 +0300

    feat: enable scroll in column

--- project-management-app/src/app/workspace/single-board-page/components/board-column/board-column.component.ts
+++ project-management-app/src/app/workspace/single-board-page/components/board-column/board-column.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { ColumnModel } from 'src/app/workspace/board-list-page/models/board.model';
 
 @Component({
@@ -6,6 +6,18 @@ import { ColumnModel } from 'src/app/workspace/board-list-page/models/board.mode
   templateUrl: './board-column.component.html',
   styleUrls: ['./board-column.component.scss'],
 })
-export class BoardColumnComponent {
+export class BoardColumnComponent implements OnInit {
   @Input() column: ColumnModel;
+
+  public title: string;
+
+  ngOnInit() {
+    this.title = this.column.title;
+  }
+
+  editColumnTitle(input: EventTarget | null) {
+    if (input) {
+      (<HTMLInputElement>input).readOnly = !(<HTMLInputElement>input).readOnly;
+    }
+  }
 }
