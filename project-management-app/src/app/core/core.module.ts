4eb5bc16901afd1b1b4040822aae5ed3e9f55c1b
Pavel Kizhlo <pavelkizh@gmail.com>
2022-11-10 22:36:10 +0300

    feat: register root state

--- project-management-app/src/app/core/core.module.ts
+++ project-management-app/src/app/core/core.module.ts
@@ -4,20 +4,37 @@ import { StoreModule } from '@ngrx/store';
 import { EffectsModule } from '@ngrx/effects';
 
 import { SharedModule } from '../shared/shared.module';
+import { AuthModule } from '../authorization/auth.module';
 import { StartScreenModule } from '../start-screen/start-screen.module';
 import { WorkspaceModule } from '../workspace/workspace.module';
 import { CommonInterceptor } from './services/common.interceptor';
+import { AuthEffects } from './store/effects/auth.effects';
+import { BoardEffects } from './store/effects/board.effects';
+import { ColumnEffects } from './store/effects/column.effects';
+import { TaskEffects } from './store/effects/task.effects';
+import { UserEffects } from './store/effects/user.effects';
+import { authReducer } from './store/reducers/auth.reducer';
+import { userReducer } from './store/reducers/user.reducer';
+import { boardReducer } from './store/reducers/board.reducer';
+import { columnReducer } from './store/reducers/column.reducer';
+import { taskReducer } from './store/reducers/task.reducer';
+import { localizationReducer } from './store/reducers/localization.reducer';
 
 const COMMON_INTERCEPTOR = { provide: HTTP_INTERCEPTORS, useClass: CommonInterceptor, multi: true };
-import { EffectsModule } from '@ngrx/effects';
-import { AuthModule } from '../authorization/auth.module';
 
 @NgModule({
   declarations: [],
   imports: [
     HttpClientModule,
-    StoreModule.forRoot({}, {}),
-    EffectsModule.forRoot([]),
+    StoreModule.forRoot({
+      auth: authReducer,
+      user: userReducer,
+      board: boardReducer,
+      column: columnReducer,
+      task: taskReducer,
+      localization: localizationReducer,
+    }),
+    EffectsModule.forRoot([AuthEffects, BoardEffects, ColumnEffects, TaskEffects, UserEffects]),
     SharedModule,
     StartScreenModule,
     WorkspaceModule,
---
+++ project-management-app/src/app/core/store/selectos/app.selectors.ts
@@ -0,0 +1,7 @@
+// import { createSelector } from '@ngrx/store';
+
+// import { StateModel } from '../state/state.model';
+
+// const selectBoards = (state: StateModel) => state.boards;
+
+// export const selectColumns = createSelector(selectBoards ,);
