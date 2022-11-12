71e07fb6e45ac4dd62daeba28d4975ae24ae2de7
rybakN <dnb4949@gmail.com>
2022-11-12 23:37:22 +0300

    feat: get token from State, unsubscribe

--- project-management-app/src/app/core/services/token.interceptor.ts
+++ project-management-app/src/app/core/services/token.interceptor.ts
@@ -1,13 +1,15 @@
-import { Injectable } from '@angular/core';
+import { Injectable, OnDestroy } from '@angular/core';
 import { HttpRequest, HttpHandler, HttpEvent, HttpInterceptor } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { Observable, Subscription, take } from 'rxjs';
 import { Store } from '@ngrx/store';
 import { StateModel } from '../store/state/state.model';
 import { selectToken } from '../store/selectos/app.selectors';
 
 @Injectable()
-export class TokenInterceptor implements HttpInterceptor {
-  token$: Observable<string | null>;
+export class TokenInterceptor implements HttpInterceptor, OnDestroy {
+  token: string | null;
+
+  private subscription$: Subscription;
 
   constructor(private store: Store<StateModel>) {}
 
@@ -15,9 +17,25 @@ export class TokenInterceptor implements HttpInterceptor {
     if (req.url.includes('/signin' || '/signup' || '/logs')) {
       return next.handle(req);
     } else {
-      this.token$ = this.store.select(selectToken);
-      const cloned = req.clone({ body: { ...req.body, token: this.token$ } });
-      return next.handle(cloned);
+      this.getToken();
+      return next.handle(
+        req.clone({
+          setHeaders: {
+            authorization: `Bearer ${this.token}`,
+          },
+        }),
+      );
     }
+  }
+
+  private getToken() {
+    this.subscription$ = this.store
+      .select(selectToken)
+      .pipe(take(1))
+      .subscribe((value) => (this.token = value));
+  }
+
+  ngOnDestroy() {
+    this.subscription$.unsubscribe();
   }
 }
