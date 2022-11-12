4891fba5077b1216ec6c21040de15e4dd1ca7b22
rybakN <dnb4949@gmail.com>
2022-11-12 14:51:48 +0300

    refactor: add in component async pipe

--- project-management-app/src/app/workspace/single-board-page/single-board-page.component.ts
+++ project-management-app/src/app/workspace/single-board-page/single-board-page.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { CdkDragDrop, moveItemInArray } from '@angular/cdk/drag-drop';
 
@@ -7,34 +7,26 @@ import { StateModel } from 'src/app/core/store/state/state.model';
 import { selectBoardById } from 'src/app/core/store/selectos/app.selectors';
 import { BoardModel } from 'src/app/shared/models/board.model';
 import { ColumnModel } from 'src/app/shared/models/column.model';
-import { Subscription } from 'rxjs';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-single-board-page',
   templateUrl: './single-board-page.component.html',
   styleUrls: ['./single-board-page.component.scss', './drag&drop.scss'],
 })
-export class SingleBoardPageComponent implements OnInit, OnDestroy {
-  public board: BoardModel;
+export class SingleBoardPageComponent implements OnInit {
+  public board$: Observable<BoardModel>;
 
-  private boardSubscribtion: Subscription;
-
-  // eslint-disable-next-line @ngrx/no-typed-global-store
   constructor(public route: ActivatedRoute, private store: Store<StateModel>) {}
 
   ngOnInit(): void {
     this.route.params.subscribe((params) => {
       const boardId = params['id'];
-      // eslint-disable-next-line @ngrx/no-store-subscription
-      this.store.select(selectBoardById(boardId)).subscribe((board) => (this.board = board));
+      this.board$ = this.store.select(selectBoardById(boardId));
     });
   }
 
-  ngOnDestroy(): void {
-    this.boardSubscribtion.unsubscribe();
-  }
-
-  drop(event: CdkDragDrop<ColumnModel[]>) {
-    moveItemInArray(this.board.columns!, event.previousIndex, event.currentIndex);
+  drop(event: CdkDragDrop<ColumnModel[]>, board: BoardModel) {
+    moveItemInArray(board.columns!, event.previousIndex, event.currentIndex);
   }
 }
