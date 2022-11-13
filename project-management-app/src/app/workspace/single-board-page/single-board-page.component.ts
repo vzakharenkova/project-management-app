533d352ab01bb203f1060df52a81b9fbbe919586
vzakharenkova <v.s.zakharenkova@gmail.com>
2022-11-13 19:20:42 +0300

    feat: create new column

--- project-management-app/src/app/workspace/single-board-page/single-board-page.component.ts
+++ project-management-app/src/app/workspace/single-board-page/single-board-page.component.ts
@@ -4,10 +4,13 @@ import { CdkDragDrop, moveItemInArray } from '@angular/cdk/drag-drop';
 
 import { Store } from '@ngrx/store';
 import { StateModel } from 'src/app/core/store/state/state.model';
-import { selectBoardById } from 'src/app/core/store/selectos/app.selectors';
 import { BoardModel } from 'src/app/shared/models/board.model';
 import { ColumnModel } from 'src/app/shared/models/column.model';
 import { Observable } from 'rxjs';
+import { MatDialog } from '@angular/material/dialog';
+import { CreateColumnComponent } from './components/create-column/create-column.component';
+import { selectCurrentBoard } from 'src/app/core/store/selectos/app.selectors';
+import { getBoardById } from 'src/app/core/store/actions/board.actions';
 
 @Component({
   selector: 'app-single-board-page',
@@ -17,16 +20,32 @@ import { Observable } from 'rxjs';
 export class SingleBoardPageComponent implements OnInit {
   public board$: Observable<BoardModel>;
 
-  constructor(public route: ActivatedRoute, private store: Store<StateModel>) {}
+  private boardId: string;
+
+  constructor(
+    public route: ActivatedRoute,
+    private store: Store<StateModel>,
+    public dialog: MatDialog,
+  ) {}
 
   ngOnInit(): void {
     this.route.params.subscribe((params) => {
       const boardId = params['id'];
-      this.board$ = this.store.select(selectBoardById(boardId));
+      this.boardId = boardId;
     });
+    this.store.dispatch(getBoardById({ boardId: this.boardId }));
+    this.board$ = this.store.select(selectCurrentBoard);
   }
 
   drop(event: CdkDragDrop<ColumnModel[]>, board: BoardModel) {
     moveItemInArray(board.columns!, event.previousIndex, event.currentIndex);
+  }
+
+  openCreateColumnForm() {
+    this.dialog.open(CreateColumnComponent, {
+      data: {
+        boardId: this.boardId,
+      },
+    });
   }
 }
