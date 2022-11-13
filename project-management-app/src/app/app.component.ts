ecc783f9bb88b3facb06faee99e816bdb3bacaf4
vzakharenkova <v.s.zakharenkova@gmail.com>
2022-11-13 14:49:52 +0300

    feat: get login from LS

--- project-management-app/src/app/app.component.ts
+++ project-management-app/src/app/app.component.ts
@@ -1,13 +1,27 @@
 import { Component } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { signedIn } from './core/store/actions/auth-api.actions';
+import { StateModel } from './core/store/state/state.model';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'project-management-app';
+
+  constructor(private store: Store<StateModel>) {}
+
+  ngOnInit(): void {
+    const token = localStorage.getItem('token');
+
+    if (token) {
+      this.store.dispatch(
+        signedIn({
+          tokenObj: { token },
+        }),
+      );
+    }
+  }
 }
-
-
-
