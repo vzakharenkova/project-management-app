c791918198faf2137c5c494ba9b0aa69bc1ebd91
vzakharenkova <v.s.zakharenkova@gmail.com>
2022-11-23 22:07:35 +0300

    fix: change url

--- project-management-app/src/app/core/services/common.interceptor.ts
+++ project-management-app/src/app/core/services/common.interceptor.ts
@@ -1,20 +1,13 @@
-import {
-  HttpEvent,
-  HttpHandler,
-  HttpHeaders,
-  HttpInterceptor,
-  HttpRequest,
-} from '@angular/common/http';
+import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
 export class CommonInterceptor implements HttpInterceptor {
   // BASE_URL = 'http://ec2-3-80-45-34.compute-1.amazonaws.com';
-  BASE_URL = 'http://34.88.100.156';
+  BASE_URL = '//34.88.100.156';
 
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-    const headers = new HttpHeaders({ referrerPolicy: 'unsafe_url' });
     if (!req.url.includes('/assets/i18n/')) {
-      const cloned = req.clone({ url: this.BASE_URL + req.url, headers });
+      const cloned = req.clone({ url: this.BASE_URL + req.url });
       return next.handle(cloned);
     } else {
       return next.handle(req);
