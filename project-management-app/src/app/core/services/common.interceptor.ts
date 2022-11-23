ad844dd79285a9a64e1d14a483163c0158a8ca0a
vzakharenkova <v.s.zakharenkova@gmail.com>
2022-11-23 21:52:22 +0300

    fix: change url

--- project-management-app/src/app/core/services/common.interceptor.ts
+++ project-management-app/src/app/core/services/common.interceptor.ts
@@ -3,7 +3,7 @@ import { Observable } from 'rxjs';
 
 export class CommonInterceptor implements HttpInterceptor {
   // BASE_URL = 'http://ec2-3-80-45-34.compute-1.amazonaws.com';
-  BASE_URL = '//34.88.100.156';
+  BASE_URL = 'http://34.88.100.156';
 
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
     if (!req.url.includes('/assets/i18n/')) {
